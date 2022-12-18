8ae0e514a4655acba34a8a9d09a2988440b50c75
Eriki Enos <Erikjenos@hotmail.com>
2022-12-17 20:18:21 -0800

    Final Build

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -7,27 +7,33 @@ public class GameLogic : MonoBehaviour
     float durationUntilNextBalloon;
     Sprite circleTexture;
 
+    LinkedList<GameObject> activeBalloons;
+
     void Start()
     {
         NetworkedClientProcessing.SetGameLogic(this);
+        activeBalloons = new LinkedList<GameObject>();
+
     }
     void Update()
     {
-        durationUntilNextBalloon -= Time.deltaTime;
+        //durationUntilNextBalloon -= Time.deltaTime;
 
-        if(durationUntilNextBalloon < 0)
-        {
-            durationUntilNextBalloon = 1f;
+        //if(durationUntilNextBalloon < 0)
+        //{
+        //    durationUntilNextBalloon = 1f;
 
-            float screenPositionXPercent = Random.Range(0.0f, 1.0f);
-            float screenPositionYPercent = Random.Range(0.0f, 1.0f);
-            Vector2 screenPosition = new Vector2(screenPositionXPercent * (float)Screen.width, screenPositionYPercent * (float)Screen.height);
-            SpawnNewBalloon(screenPosition);
-        }
+        //    float screenPositionXPercent = Random.Range(0.0f, 1.0f);
+        //    float screenPositionYPercent = Random.Range(0.0f, 1.0f);
+        //    Vector2 screenPosition = new Vector2(screenPositionXPercent * (float)Screen.width, screenPositionYPercent * (float)Screen.height);
+        //    SpawnNewBalloon(screenPosition);
+        //}
     }
-    public void SpawnNewBalloon(Vector2 screenPosition)
+    public void SpawnNewBalloon(float xPosPercent, float yPosPercent, int balloonID)
     {
-        if(circleTexture == null)
+        Vector2 screenPosition = new Vector2(xPosPercent * (float)Screen.width, yPosPercent * (float)Screen.height);
+
+        if (circleTexture == null)
             circleTexture = Resources.Load<Sprite>("Circle");
 
         GameObject balloon = new GameObject("Balloon");
@@ -41,6 +47,28 @@ public class GameLogic : MonoBehaviour
         pos.z = 0;
         balloon.transform.position = pos;
         //go.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(screenPosition.x, screenPosition.y, -Camera.main.transform.position.z));
+
+        balloon.GetComponent<CircleClick>().balloonID = balloonID;
+        activeBalloons.AddLast(balloon);
+    }
+
+    public void BalloonWaPopped(int balloonID)
+    {
+        GameObject destoryMe = null;
+        foreach(GameObject b in activeBalloons)
+        {
+            if(b.GetComponent<CircleClick>().balloonID == balloonID)
+            {
+                destoryMe = b;
+                break;
+            }
+        }
+
+        if(destoryMe != null)
+        {
+            activeBalloons.Remove(destoryMe);
+            Destroy(destoryMe);
+        }
     }
 
 }
